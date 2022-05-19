9c123f7909fbceb0f85d964e60ecec52cf4350ba
Fatih Tufan <36725294+fatihtufan@users.noreply.github.com>
2022-05-19 18:15:09 +0300

    #15 Repositorylerin Yazıldı

---
+++ Blog.DataAccessLayer/Abstract/IArticleDal.cs
@@ -0,0 +1,18 @@
+﻿using Blog.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.DataAccessLayer.Abstract
+{
+    public interface IArticleDal
+    {
+        List<Article> ListAllArticle();
+        void AddArticle(Article article);
+        void DeleteArticle(Article article);
+        void UpdateArticle(Article article);
+        Article GetById(int id);
+    }
+}
---
+++ Blog.DataAccessLayer/Repositories/CategoryRepository.cs
@@ -0,0 +1,43 @@
+﻿using Blog.DataAccessLayer.Abstract;
+using Blog.DataAccessLayer.Concrete;
+using Blog.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.DataAccessLayer.Repositories
+{
+    internal class CategoryRepository : ICategoryDal
+    {
+        Context c = new Context();
+        public void AddCategory(Category category)
+        {
+            c.Add(category);
+            c.SaveChanges();
+        }
+
+        public void DeleteCategory(Category category)
+        {
+            c.Remove(category);
+            c.SaveChanges();
+        }
+
+        public Category GetById(int id)
+        {
+            return c.Catogries.Find(id);
+        }
+
+        public List<Category> ListAllCategory()
+        {
+            return c.Catogries.ToList();
+        }
+
+        public void UpdateCategory(Category category)
+        {
+            c.Update(category);
+            c.SaveChanges();
+        }
+    }
+}
