0d4905bcc63298b73335ae174cbaa45b0fbbe644
Fatih Tufan <36725294+fatihtufan@users.noreply.github.com>
2022-06-02 21:37:04 +0300

    Projeye tema eklendi

---
+++ Blog.MvcUI/Controllers/ArticleController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Blog.MvcUI.Controllers
+{
+    public class ArticleController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
